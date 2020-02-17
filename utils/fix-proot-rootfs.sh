c1c0855c770e490c8827ffa07353fbd6a25d70a2
green-green-avk <45503261+green-green-avk@users.noreply.github.com>
2020-02-16 21:37:10 -0800

    Some utils.

---
+++ utils/fix-proot-rootfs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+OP='.l2s.'
+P='.proot'
+
+if [ -n "$2" ] ; then
+shift
+prepend() {
+echo "$(dirname "$1")/$P$(basename "$1")"
+}
+relink() {
+[ -L "$1" ] || return 0
+local L="$(readlink "$1")"
+[[ $(basename "$L") = $OP* ]] && { echo "Relinking: $1 -> $L" ; ln -snf "$(prepend "$L")" "$1" ; }
+}
+relink "$1"
+[[ $(basename "$1") = $OP* ]] && { echo " Renaming: $1" ; mv -Tf "$1" "$(prepend "$1")" ; }
+exit 0
+fi
+
+find -P "${1:-.}" \( -name "$OP"\* -or -type l \) -exec "$0" - '{}' \;
